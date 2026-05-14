1af96baf36cf338c7e65f036472204735eab058e
akashsi126 <martonline.akashsinghal@gmail.com>
2026-05-14 23:43:02 +0530

    Refactor className for h2 element in index-C78NrZjj.js

--- assets/index-C78NrZjj.js
+++ assets/index-C78NrZjj.js
@@ -16941,7 +16941,7 @@ function y2() {
                   m.jsx(E.h2, {
                     variants: d,
                     className:
-                      "text-3xl font-['Allura',_cursive] text-[#B3385A]",
+                      "font-allura text-3xl text-[#B3385A]",
                     children: "Tanmay",
                   }),
                   m.jsx(E.p, {
